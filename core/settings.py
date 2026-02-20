e82b1d190830277257c1615194ff347c46c801b2
Santiago Huelgos <shuelgos4@gmail.com>
2026-02-19 21:52:14 -0500

    feat(api): add token authentication, pagination, filtering, search and ordering to products endpoint

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'inventory'
+    'inventory',
+    'django_filters',
+    'rest_framework.authtoken',
 
 ]
 
@@ -118,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
--- inventory/views.py
+++ inventory/views.py
@@ -1,12 +1,22 @@
 from rest_framework import viewsets
 from .models import Product, StockMovement
 from .serializers import ProductSerializer, StockMovementSerializer
+from .pagination import StandardResultsSetPagination
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset=Product.objects.filter(is_active=True)
+    queryset=Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
+    filterset_fields=['name','is_active','minimum_stock']
+    search_fields=['name','description']
+    ordering_fields=['name','minimum_stock','created_at']
+    ordering=['-created_at']
     
 class StockMovementViewSet (viewsets.ModelViewSet):
     queryset=StockMovement.objects.all()
-    serializer_class = StockMovementSerializer    
+    serializer_class = StockMovementSerializer
+    pagination_class = StandardResultsSetPagination    
+    
+    
+    
     
