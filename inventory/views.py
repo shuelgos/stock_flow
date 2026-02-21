0949c51704b9b2d237985ad5ed464e811a7679d7
Santiago Huelgos <shuelgos4@gmail.com>
2026-02-20 22:25:39 -0500

    feat: add inventory summary endpoint and improve product metrics

--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from .models import Product, StockMovement
 from .serializers import ProductSerializer, StockMovementSerializer
 from .pagination import StandardResultsSetPagination
+from .permissions import IsAdminOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset=Product.objects.all()
@@ -11,6 +15,42 @@ class ProductViewSet(viewsets.ModelViewSet):
     search_fields=['name','description']
     ordering_fields=['name','minimum_stock','created_at']
     ordering=['-created_at']
+    permission_classes= [IsAdminOrReadOnly]
+    
+    @action(detail=False,methods=['get'])
+    def low_stock(self,request):
+        products=self.get_queryset()
+        
+        low_stock_products=[
+            p for p in products
+            if p.current_stock < p.minimum_stock
+        ]
+        
+        serializer=self.get_serializer(low_stock_products,many=True)
+        return Response (serializer.data)
+        
+    @action (detail=False,methods=['get'])
+    def summary(self,request):
+        products=self.get_queryset() 
+        
+        low_stock_products = [
+        p for p in products
+        if p.current_stock < p.minimum_stock
+        ]
+        total_products= products.count()
+        active_products= products.filter(is_active=True).count()
+        low_stock_count=len(low_stock_products)
+        total_stock_units = sum(p.current_stock for p in products)
+        
+        data = {
+            "total_products": total_products,
+            "active_products": active_products,
+            "low_stock_products": low_stock_count,
+            "total_stock_units": total_stock_units,
+        }
+        
+        return Response(data)
+              
     
 class StockMovementViewSet (viewsets.ModelViewSet):
     queryset=StockMovement.objects.all()
